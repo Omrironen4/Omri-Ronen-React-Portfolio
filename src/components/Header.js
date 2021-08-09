9e239404c73699259f30d3b06fe6f7241527d9a9
Omri <omri.ronen4@gmail.com>
2021-08-08 20:39:26 -0700

    finalized Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from './Navigation';
+import SideNav from './SideNav';
+
+const Header = ({page, handlePageChange}) => {
+    return (
+        <div>
+            <div className="navbar-fixed">
+                <Navigation page={page} handlePageChange={handlePageChange}/>
+            </div>
+            <SideNav handlePageChange={handlePageChange}/>
+        </div>
+    );
+}
+
+export default Header;
