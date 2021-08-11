60f3f01d0e28ec543631d4cde1aba446474f6747
Omri <omri.ronen4@gmail.com>
2021-08-11 16:52:28 -0700

    cleaned up format for Home.js and finsihed the Contact.js file

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        // <!-- 1st row -->
+        <main>
+            <div className="row">
+            <div className="center col s12 m12 l12">
+                <p className="title" id="contact">
+                    Contact me</p>
+            </div>
+        </div>
+        {/* <!-- 2nd row --> */}
+        <div className="row">
+            <div className="center col s12 m12 l12">
+                <ul className="contact-info">
+                    <li>Name: Omri Ronen</li>
+                    <li>Email: <a
+                            className="email-color"
+                            href="mailto:omri.ronen4@gmail.com">
+                            omri.ronen4@gmail.com</a>
+                    </li>
+                    <li>Phone: (949)-910-7806</li>
+                </ul>
+            </div>
+        </div>
+        </main>
+    )
+}
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,13 +5,13 @@ export default function Home() {
     return (
         <main>
             <div>
-            <div className="intro-image">
-                <img className="responsive-img"
-                    src={kauaiPic} />
+                <div className="intro-image">
+                    <img className="responsive-img"
+                        src={kauaiPic} />
+                </div>
+                <h5 className="brand-logo welcome">Welcome
+                    to my portfolio</h5>
             </div>
-            <h5 className="brand-logo welcome">Welcome
-                to my portfolio</h5>
-        </div>
         </main>
     );
 }
