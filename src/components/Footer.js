19b7413b1326c975b2e366aee231bbb317eefcbf
Omri <omri.ronen4@gmail.com>
2021-11-22 19:26:13 +0200

    ok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,20 @@ import React from 'react';
 const Footer = () => {
     return (
         <main>
-
             <footer id="footinfo" className="page-footer">
-                <div className="container">
-                    <div className="row">
-
-                        <div className="col l4 offset-l2 s12">
-                            <ul className="left">
-                                <li><a className="grey-text text-lighten-3"
-                                    href="https://github.com/Omrironen4" target="_blank"  rel="noreferrer">GitHub</a>
-                                </li>
-                                <li><a className="grey-text text-lighten-3"
-                                    href="https://www.linkedin.com/in/omri-ronen-a72766206/" target="_blank" rel="noreferrer" >LinkedIn
-                                </a></li>
-                                <li><a className="grey-text text-lighten-3"
-                                    href="https://drive.google.com/file/d/106UkBzYzoWFOG0kt0m1K1aPvxABlSQDC/view?usp=sharing" download target="_blank" rel="noreferrer">Resume
-                                </a></li>
-
-
-                            </ul>
-                        </div>
+                <div className='container'>
+                    <div className="col l4 offset-l2 s12">
+                        <ul className="left">
+                            <li><a className="grey-text text-lighten-3"
+                                href="https://github.com/Omrironen4" target="_blank" rel="noreferrer">GitHub</a>
+                            </li>
+                            <li><a className="grey-text text-lighten-3"
+                                href="https://www.linkedin.com/in/omri-ronen-a72766206/" target="_blank" rel="noreferrer" >LinkedIn
+                            </a></li>
+                            <li><a className="grey-text text-lighten-3"
+                                href="https://drive.google.com/file/d/106UkBzYzoWFOG0kt0m1K1aPvxABlSQDC/view?usp=sharing" download target="_blank" rel="noreferrer">Resume
+                            </a></li>
+                        </ul>
                     </div>
                 </div>
             </footer>
@@ -31,3 +25,7 @@ const Footer = () => {
 }
 
 export default Footer;
+
+
+
+
